3824a74d23bc12d2cff1f1dbf27268b6343565de
Red Rubber Duck <122934358+roman-duck@users.noreply.github.com>
2024-10-04 21:14:58 +0300

    Available tickets, close #1

--- src/Railway.php
+++ src/Railway.php
@@ -1,8 +1,40 @@
 <?php
 
 namespace App;
+
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
 class Railway
 {
+    private HttpClientInterface $client;
+
+    const HOST = "app.uz.gov.ua";
+
+    const SCHEME = "https";
+
+    private string $baseUrl;
+
+    public function __construct(HttpClientInterface $client, User $user)
+    {
+        $this->client = $client->withOptions([
+            'headers' => [
+                'Accept' => 'application/json',
+                'Authorization' => sprintf('Bearer %s', $user->getToken()),
+                'x-user-agent' => sprintf('UZ/2 Web/1 User/%d', $user->getId()),
+            ]
+        ]);
+        $this->baseUrl = sprintf('%s://%s', self::SCHEME, self::HOST);
+    }
+
+    public function getTrips(\DateTime $date, int $stationFrom, int $stationTo): array
+    {
+        $endpoint = '/api/v3/trips';
+        $pattern = '%s%s?station_from_id=%d&station_to_id=%d&with_transfers=0&date=%s';
+        $uri = sprintf($pattern, $this->baseUrl, $endpoint, $stationFrom, $stationTo, $date->format('Y-m-d'));
+        $response = $this->client->request('GET', $uri);
+        return $response->toArray();
+    }
+
     public function getVersion(): string
     {
         return '1.0.0';
--- tests/RailwayTest.php
+++ tests/RailwayTest.php
@@ -2,14 +2,27 @@
 
 namespace App\Tests;
 
-use PHPUnit\Framework\TestCase;
+use App\User;
 use App\Railway;
+use Symfony\Component\HttpClient\MockHttpClient;
+use Symfony\Component\HttpClient\Response\MockResponse;
+use PHPUnit\Framework\TestCase;
 
 class RailwayTest extends TestCase
 {
     public function testGetVersion()
     {
-        $railway = new Railway();
+        $responses = [
+            new MockResponse('{"version":"1.0.0"}', []),
+        ];
+
+        $httpClient = new MockHttpClient($responses);
+        $user = $this->createMock(User::class);
+        $user->method('getId')
+            ->willReturn(1);
+        $user->method('getToken')->willReturn(sha1('token'));
+
+        $railway = new Railway($httpClient, $user);
         $this->assertEquals('1.0.0', $railway->getVersion());
     }
 }
