40748e6df3666aa3fcd0e1721f7a88f0aafa7af2
Red Rubber Duck <mvch@triangle.software>
2024-10-24 23:46:58 +0300

    fixes

--- server/index.php
+++ server/index.php
@@ -5,7 +5,11 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\Dotenv\Dotenv;
 use App\TgHttpClient;
+
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__.'/../.env');
 
 $request = Request::createFromGlobals();
 
@@ -17,9 +21,10 @@ $response = new JsonResponse([
 ], 404);
 
 $steps = [
-    'from' => 'Enter the departure station:',
-    'to' => 'Enter the arrival station:',
-    'date' => 'Enter the date of the trip:',
+    'Enter the departure station:',
+    'Enter the arrival station:',
+    'Enter the date of the trip:',
+    'Searching for available seats...',
 ];
 
 $trip = [];
@@ -28,44 +33,44 @@ if ($uri === '/bot' && $request->isMethod('POST')) {
     $client = HttpClient::create();
     $tg = new TgHttpClient($client, $_ENV['TG_TOKEN']);
 
-    session_start();
-    $currentStep = $_SESSION['current_step'] ?? 0;
-
     $data = $request->toArray();
     $text = $data['message']['text'];
     $chatId = $data['message']['from']['id'];
+
+    $db = new \SQLite3(__DIR__ . '/../var/data/local.db', SQLITE3_OPEN_READWRITE);
+    $db->enableExceptions(true);
+    $statement = $db->prepare('SELECT step FROM messages WHERE chat_id = :chat_id ORDER BY id DESC LIMIT 1');
+    $statement->bindValue(':chat_id', $chatId, SQLITE3_INTEGER);
+    $res = $statement->execute();
+
+    $record = $res->fetchArray(SQLITE3_ASSOC);
+    $currentStep = $record['step'] ?? 0;
 
     if (str_starts_with($text, '/start')) {
         $currentStep = 0;
     }
 
-    $db = new \SQLite3(__DIR__ . '/../var/data/local.db', SQLITE3_OPEN_READWRITE);
-    $db->enableExceptions(true);
-    $db->query('INSERT INTO messages("chat_id", "user_id", "body", "step") VALUES ("'.$chatId.'", "1", "'.$text.'", "'.$currentStep.'")');
-    $db->close();
-
     $tg->sendMessage($chatId, $steps[$currentStep]);
 
-    if ($currentStep === 0) {
-        $currentStep++;
+    switch ($currentStep) {
+        case 0:
+            $trip['from'] = $text;
+            $currentStep++;
+            break;
+        case 1:
+            $trip['to'] = $text;
+            $currentStep++;
+            break;
+        case 2:
+            $trip['date'] = $text;
+            $currentStep++;
+            break;
+        default:
+            break;
     }
 
-    if ($currentStep === 1) {
-        $trip['from'] = $text;
-        $currentStep++;
-    }
-
-    if ($currentStep === 2) {
-        $trip['to'] = $text;
-        $currentStep++;
-    }
-
-    if ($currentStep === 3) {
-        $trip['date'] = $text;
-        $currentStep++;
-    }
-
-    $_SESSION['current_step'] = $currentStep;
+    $db->query('INSERT INTO messages("chat_id", "user_id", "body", "step") VALUES ("'.$chatId.'", "1", "'.$text.'", "'.$currentStep.'")');
+    $db->close();
 
     $response = new JsonResponse([
         'status' => 'ok',
